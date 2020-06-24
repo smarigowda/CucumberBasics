58258f97283a8c307d72a9ff1e2b75ff47a7004f
Santosh Marigowda <santosharakere@gmail.com>
2020-06-24 22:51:13 +0100

    first commit

---
+++ src/main/java/com/tryonyourown/cucumber/basics/App.java
@@ -0,0 +1,12 @@
+package com.tryonyourown.cucumber.basics;
+
+/**
+ * Hello Cucumber world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
